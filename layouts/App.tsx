c5be12c8db270b68ca5d10b372c1635c75d2a1fa
jmg <jmg41490400@gmail.com>
2022-06-14 17:19:29 +0900

    add router

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-const App = () => {
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
+import { Navigate, Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
+const App: FC = () => {
   return (
-    <div>
-      <div>App</div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Navigate to={'/login'} replace />} />
+        <Route path={'/login'} element={<Login />} />
+        <Route path={'/signup'} element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default App;
