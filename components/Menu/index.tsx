a1fd34c0a54ca37359ad3a0edd3688e34bdabc57
jmg <jmg41490400@gmail.com>
2022-06-15 18:26:54 +0900

    profile menu 만들기

---
+++ components/Menu/index.tsx
@@ -0,0 +1,29 @@
+import React, { CSSProperties, useCallback } from 'react';
+import { CloseModalButton, CreateMenu } from '@components/Menu/styles';
+
+type Props = {
+  children?: React.ReactNode;
+  show: boolean;
+  onCloseModal: () => void;
+  style: CSSProperties;
+  closeButton?: boolean;
+};
+const Menu: React.FC<Props> = ({ closeButton, style, show, children, onCloseModal }: Props) => {
+  // 부모 영역을 눌렀을땐 내가 닫히게
+  // 나를 눌렀을땐 내가 안닫히게
+  const stopPropagation = useCallback((e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+  }, []);
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div onClick={stopPropagation} style={style}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
+};
+Menu.defaultProps = {
+  closeButton: true,
+};
+export default Menu;
--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import axios from 'axios';
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
@@ -7,8 +7,10 @@ import {
   Channels,
   Chats,
   Header,
+  LogOutButton,
   MenuScroll,
   ProfileImg,
+  ProfileModal,
   RightMenu,
   WorkspaceName,
   Workspaces,
@@ -17,18 +19,22 @@ import {
 import gravatar from 'gravatar';
 import { Route, Routes } from 'react-router';
 import loadable from '@loadable/component';
+import Menu from '@components/Menu';
 
 const Channel = loadable(() => import('@pages/channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const Index = () => {
   const { data, error, mutate } = useSWR('/api/users', fetcher);
+  const [showUserMenu, setShowUserMenu] = useState(false);
   const onLogout = useCallback(() => {
     axios.post('/api/users/logout', null, { withCredentials: true }).then(() => {
       mutate(false, false);
     });
   }, []);
-
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu((prev) => !prev);
+  }, []);
   // return 은 hooks 보다 아래에 있어야함
   if (!data) {
     return <Navigate replace to="/login" />;
@@ -37,12 +43,23 @@ const Index = () => {
     <div>
       <Header>
         <RightMenu>
-          <span>
+          <span onClick={onClickUserProfile}>
             <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+            {showUserMenu && (
+              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                <ProfileModal>
+                  <img src={gravatar.url(data.email, { s: '36px', d: 'retro' })} alt={data.nickname} />
+                  <div>
+                    <span id={'profile-name'}>{data.nickname}</span>
+                    <span id={'profile-active'}>Active</span>
+                  </div>
+                </ProfileModal>
+                <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+              </Menu>
+            )}
           </span>
         </RightMenu>
       </Header>
-      <button onClick={onLogout}>로그아웃</button>
       <WorkspaceWrapper>
         <Workspaces>test</Workspaces>
         <Channels>
